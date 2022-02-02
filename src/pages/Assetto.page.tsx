cef50a8d897c7cd7b5df4e30b3efad5656036181
Leonardo Pivetta <leonardo.pivetta101@gmail.com>
2022-02-02 15:17:56 +0100

    edit the viewing of the setup

--- src/pages/Assetto.page.tsx
+++ src/pages/Assetto.page.tsx
@@ -1,110 +1,66 @@
-import { Setup } from "../types/Setup"
+import { Setup, TyresValues } from "../types/Setup"
 import { useEffect, useState } from "react"
 import { getSetup } from "../firebase/firestore"
 import { Timestamp } from "firebase/firestore"
 
-const TopView: React.FC<{setup: Setup}> = props => {
+/**
+ * @param setup
+ * @returns JSX element with the render of the top view of the car for the setup
+ */
+
+const TopView: React.FC<{ setup: Setup }> = props => {
     return <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 538.79 479.82" className="h-full w-full mx-auto my-auto"
-        >
-            <defs>
-                <style>
-                    {
-                        ".cls-1,.cls-3,.cls-4{fill:none;stroke:#f6f6f6;stroke-miterlimit:10}.cls-2,.cls-5{fill:#fff}.cls-3{stroke-dasharray:11.7 11.7}.cls-4{stroke-dasharray:11.32 11.32}.cls-5{font-size:12px;}.cls-6{letter-spacing:-.01em}"
-                    }
-                </style>
-            </defs>
-            <g id="Livello_2" data-name="Livello 2">
-                <g id="Livello_1-2" data-name="Livello 1">
-                    <path
-                        className="cls-1"
-                        d="M266.34 479.18s47 5.08 61.35-46.59S360.72 253 359 167.46 298.89 4.83 266.34 4c-32.55.85-91 77.93-92.69 163.48S190.59 380.92 205 432.59s61.34 46.59 61.34 46.59ZM94.78 100.71h45.74v80.47H94.78zM392.15 100.71h45.74v80.47h-45.74zM79.24 364.71h76.83V464.8H79.24zM376.61 364.71h76.83V464.8h-76.83z"
-                    />
-                    <path className="cls-2" d="M117.65 414.76V274.04" />
-                    <path className="cls-1" d="M117.65 414.76v-6" />
-                    <path className="cls-3" d="M117.65 397.06V285.9" />
-                    <path className="cls-1" d="M117.65 280.04v-6" />
-                    <path className="cls-2" d="M116.4 414.76 42.08 274.04" />
-                    <path className="cls-1" d="m116.4 414.76-2.81-5.31" />
-                    <path className="cls-4" d="m108.31 399.45-60.78-115.1" />
-                    <path className="cls-1" d="m44.88 279.35-2.8-5.31" />
-                    <text className="cls-5" transform="translate(71.24 291.73)">
-                        {props.setup.toe.backLeft+"º"}
-                    </text>
-                    <path className="cls-2" d="M407.85 423.73V283.02" />
-                    <path className="cls-1" d="M407.85 423.73v-6" />
-                    <path className="cls-3" d="M407.85 406.03V294.87" />
-                    <path className="cls-1" d="M407.85 289.02v-6" />
-                    <path className="cls-2" d="m409.11 423.73 74.31-140.71" />
-                    <path className="cls-1" d="m409.11 423.73 2.8-5.3" />
-                    <path className="cls-4" d="m417.2 408.42 60.78-115.09" />
-                    <path className="cls-1" d="m480.62 288.32 2.8-5.3" />
-                    <text className="cls-5" transform="translate(421.01 300.7)">
-                        {props.setup.toe.backRight+"º"}
-                    </text>
-                    <path className="cls-2" d="M117.02 140.95V.23" />
-                    <path className="cls-1" d="M117.02 140.95v-6" />
-                    <path className="cls-3" d="M117.02 123.25V12.08" />
-                    <path className="cls-1" d="M117.02 6.23v-6" />
-                    <path className="cls-2" d="M115.77 140.95 41.45.23" />
-                    <path className="cls-1" d="m115.77 140.95-2.8-5.31" />
-                    <path className="cls-4" d="M107.68 125.63 46.9 10.54" />
-                    <path className="cls-1" d="M44.26 5.54 41.45.23" />
-                    <text className="cls-5" transform="translate(70.61 17.92)">
-                        {props.setup.toe.frontLeft+"º"}
-                    </text>
-                    <path className="cls-2" d="M415.02 140.95V.23" />
-                    <path className="cls-1" d="M415.02 140.95v-6" />
-                    <path className="cls-3" d="M415.02 123.25V12.08" />
-                    <path className="cls-1" d="M415.02 6.23v-6" />
-                    <path className="cls-2" d="M416.28 140.95 490.59.23" />
-                    <path className="cls-1" d="m416.28 140.95 2.8-5.31" />
-                    <path className="cls-4" d="m424.37 125.63 60.78-115.09" />
-                    <path className="cls-1" d="m487.79 5.54 2.8-5.31" />
-                    <text className="cls-5" transform="translate(428.18 17.92)">
-                        {props.setup.toe.frontRight+"º"}
-                    </text>
-                    <text className="cls-5" transform="translate(0 140.95)">
-                        <tspan className="cls-6">{"C"}</tspan>
-                        <tspan x={6.88} y={0}>
-                            {"amber FL"}
-                        </tspan>
-                    </text>
-                    <text className="cls-5" transform="translate(483.42 140.95)">
-                        <tspan className="cls-6">{"C"}</tspan>
-                        <tspan x={6.88} y={0}>
-                            {"amber FR"}
-                        </tspan>
-                    </text>
-                    <text className="cls-5" transform="translate(.43 423.73)">
-                        <tspan className="cls-6">{"Camber BL"}
-                        </tspan>
-                    </text>
-                    <text className="cls-5" transform="translate(483.86 423.73)">
-                        <tspan className="cls-6">{"C"}</tspan>
-                        <tspan x={6.88} y={0}>
-                            {"amber BR"}
-                        </tspan>
-                    </text>
-                    <path
-                        style={{
-                            strokeWidth: 4,
-                            fill: "none",
-                            stroke: "#f6f6f6",
-                            strokeMiterlimit: 10,
-                        }}
-                        d="M266.34 439.44V72.56"
-                    />
-                    <path
-                        style={{
-                            fill: "#f6f6f6",
-                        }}
-                        d="m286.28 78.4-19.94-34.54-19.95 34.54h39.89z"
-                    />
-                </g>
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 1059 752.24">
+        <defs>
+            <style>
+                {".cls-1{fill:none}.cls-1,.cls-4{stroke:#ffe14a;stroke-miterlimit:10;stroke-width:3px}.cls-2{font-size:1rem;fill:#f6f6f6;}.cls-3{letter-spacing:-.03em}.cls-4{fill:#3c3c3b}"}
+            </style>
+        </defs>
+        <g id="Livello_2" data-name="Livello 2">
+            <g id="Livello_2-2" data-name="Livello 2">
+                <image
+                    width={5760}
+                    height={2412}
+                    transform="matrix(0 -.13 .13 0 369.72 752.24)"
+                    xlinkHref={require("../assets/top_view.png")}
+                />
+                <path className="cls-1" d="M769.5 43.56h288v161.51h-288z" />
+                <text className="cls-2" transform="translate(781.92 65.63)">
+                    <tspan dy="1.2em" x={0}>{`Camber: ${props.setup.camber.frontRight}`}</tspan>
+                    <tspan dy="1.2em" x={0}>{`Toe: ${props.setup.toe.frontRight}`}</tspan>
+                </text>
+                <path className="cls-4" d="m769.5 205.07-123.11 70.02" />
+                <path className="cls-1" d="M769.5 322.92h288v161.51h-288z" />
+                <text className="cls-2" transform="translate(781.92 345)">
+                    <tspan dy="1.2em" x={0}>{`Camber: ${props.setup.camber.backRight}`}</tspan>
+                    <tspan dy="1.2em" x={0}>{`Toe: ${props.setup.toe.backRight}`}</tspan>
+                </text>
+                <path className="cls-4" d="m769.5 484.43-123.11 70.02" />
+                <path
+                    className="cls-1"
+                    transform="rotate(-180 145.5 124.315)"
+                    d="M1.5 43.56h288v161.51H1.5z"
+                />
+                <text className="cls-2" transform="translate(11.66 65.63)">
+                    <tspan dy="1.2em" x={0}>{`Camber: ${props.setup.camber.frontLeft}`}</tspan>
+                    <tspan dy="1.2em" x={0}>{`Toe: ${props.setup.toe.frontLeft}`}</tspan>
+                </text>
+                <path className="cls-4" d="m289.5 205.07 123.11 70.02" />
+                <path
+                    className="cls-1"
+                    transform="rotate(-180 145.5 403.675)"
+                    d="M1.5 322.92h288v161.51H1.5z"
+                />
+                <text className="cls-2" transform="translate(11.66 345)">
+                    <tspan dy="1.2em" x={0}>{`Camber: ${props.setup.camber.backLeft}`}</tspan>
+                    <tspan dy="1.2em" x={0}>{`Toe: ${props.setup.toe.backLeft}`}</tspan>
+                </text>
+                <path className="cls-4" d="m289.5 484.43 123.11 70.02" />
             </g>
-        </svg>
+        </g>
+    </svg>
 }
 
 export const AssettoPage = () => {
@@ -115,7 +71,7 @@ export const AssettoPage = () => {
     }, []);
     return <div className="h-screen w-screen flex flex-col">
         <div className="w-3/4 h-2/3 mx-auto">
-            {setup && <TopView setup={setup}/>}
+            {setup && <TopView setup={setup} />}
         </div>
     </div>
 }
