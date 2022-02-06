ce4f6a864daac168c7c7e72ef29e2641b0e0ca6e
Leonardo Pivetta <leonardo.pivetta101@gmail.com>
2022-02-06 19:51:53 +0100

    add options parameter

--- src/firebase/firebase_admin.ts
+++ src/firebase/firebase_admin.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { auth } from "./firebase";
 
 // The baseurl of the Firebase Admin Backend
@@ -26,13 +26,14 @@ const fullPath = (path: string) => {
  * @param path the path to append to the env varibale "ADMIN_BACKEND_URL"
  * @returns {any | undefined} undefined if there is an issue with the token generation or the data in response to the request
  */
-const adminGetRequest = async (path: string) => {
+const adminGetRequest = async (path: string, options?: AxiosRequestConfig) => {
     // Gets the tokenId from the currentUser
     const tokenID = await auth.currentUser?.getIdToken();
     // If there is an issue with the idToken generation returns undefined
     if(!tokenID) return undefined;
     // Makes the GET request to the Firebase Admin Backend
     const response = await axios.get(fullPath(path), {
+        ...options,
         headers: {
             authorization: `Bearer ${tokenID}`
         }
@@ -46,13 +47,14 @@ const adminGetRequest = async (path: string) => {
  * @param body the object to put in the post body
  * @returns {any | undefined} undefined if there is an issue with the token generation or the data in response to the request
  */
-const adminPostRequest = async (path: string, body?: object) => {
+const adminPostRequest = async (path: string, body?: object, options?: AxiosRequestConfig) => {
     // Gets the tokenId from the currentUser
     const tokenID = await auth.currentUser?.getIdToken();
     // If it's invalid or there is no currentUser it will return undefined
     if(!tokenID) return undefined;
     // Makes the POST request to the Firebase Admin Backend
     const response = await axios.post(fullPath(path), body, {
+        ...options,
         headers: {
             authorization: `Bearer ${tokenID}`
         }
