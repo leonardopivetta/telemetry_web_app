5180eeb15b81946f8ca459219650cfbcd030791a
Leonardo Pivetta <leonardo.pivetta101@gmail.com>
2022-02-07 15:33:44 +0100

    separate the pages and change the routes

--- src/pages/Admin.page.tsx
+++ src/pages/Admin.page.tsx
@@ -1,7 +1,6 @@
 import { FunctionComponent, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom";
 import {adminGetRequest} from "../firebase/firebase_admin";
-// import { lockedKeys } from "../types/User/LockedUserKeys";
-import { SingleUserPage } from "./SingleUser.page";
 
 /** Interface for simplify the usage viewing model  */
 export interface ShowUser {
@@ -27,7 +26,7 @@ const SearchBar: FunctionComponent<{text: string, setText: Function}> = props =>
  * @param setSelectedUser sets the selected user to show the popup in the parent
  * @returns JSX element of the table
  */
-const TableUsers: FunctionComponent<{filter:string, setSelectedUser: (_:ShowUser)=>void}> = props => {
+const TableUsers: FunctionComponent<{filter:string}> = props => {
     const [users, setUsers] = useState<Array<ShowUser>>([]);
     useEffect(()=>{
         // Gets the users from the Firebase Admin Backend
@@ -42,11 +41,12 @@ const TableUsers: FunctionComponent<{filter:string, setSelectedUser: (_:ShowUser
                 console.error(e);
             }
         })
-    })
+    },[]);
+    const navigate = useNavigate();
     return <div className="w-full h-full overflow-y-auto">
             {users.filter(e=> e.email.toLowerCase().includes(props.filter)).map(user => {
                     return <div className="p-1 px-2 border my-2 rounded-3xl transform hover:scale-100 duration-100 scale-98 hover:border-yellow-300"
-                        onClick={()=> props.setSelectedUser(user)} key={user.uid}>
+                        onClick={()=> navigate("./showUser/"+user.uid)} key={user.uid}>
                         <p>{user.email}</p>
                     </div>
                 })}
@@ -59,30 +59,8 @@ const TableUsers: FunctionComponent<{filter:string, setSelectedUser: (_:ShowUser
 export const AdminPage = () => {
     // State for the filter in the table
     const [textFilter, setTextFilter] = useState("");
-    // If selctedUser is different from undefined it will show up the popup
-    const [selectedUser, setSelectedUser] = useState<ShowUser | undefined>(undefined);
     return <div className="h-screen container mx-auto flex flex-col">
-        {/* <Popup open={selectedUser !== undefined} >
-            <div className="h-screen w-screen backdrop-blur-lg flex flex-col"
-                onClick={(e)=>{
-                    if(e.target !== e.currentTarget) return;
-                    setSelectedUser(undefined);
-                }}>
-                <div className="container mx-auto bg-pink-700 my-10 flex-grow rounded-3xl p-3">
-                    <div className="text-3xl" onClick={()=>setSelectedUser(undefined)}>
-                        {"<"}
-                    </div>
-                    {selectedUser && <SingleUserView user={selectedUser}/>}
-                </div>
-            </div>
-        </Popup> */}
-        {selectedUser && <SingleUserPage user={selectedUser} close={()=>{
-            setSelectedUser(undefined);
-        }}></SingleUserPage>}
-        { selectedUser === undefined && <div>
-                <SearchBar text={textFilter} setText={setTextFilter}/>
-                <TableUsers filter={textFilter} setSelectedUser={setSelectedUser}/>
-            </div>
-        }
+        <SearchBar text={textFilter} setText={setTextFilter}/>
+        <TableUsers filter={textFilter}/>
     </div>
 }
