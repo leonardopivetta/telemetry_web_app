d2db1722f23f63f695e171d40eb3c8410f06e1e9
Leonardo Pivetta <leonardo.pivetta101@gmail.com>
2022-03-02 13:29:03 +0100

    bug fix emulators

--- firebase_admin_node/firebase-service.js
+++ firebase_admin_node/firebase-service.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 const serviceAccountKey = require('./serviceAccountKey.json');
 
 // If is set to true it will connect to the emulators in the default ports
-const useEmulator = process.env["USE_EMULATORS"] || true;
+const useEmulator = process.env["USE_EMULATORS"];
 
 if(useEmulator){
     process.env['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080';
